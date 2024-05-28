ee9053318d55a78f22d77bb28b2c8e99f24bacca
Soren Topp <stopp@2-H-2.42heilbronn.de>
2024-05-28 18:23:38 +0200

    merge commit

--- src/redirect_functions/heredoc.c
+++ src/redirect_functions/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: stopp <stopp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 16:06:36 by stopp             #+#    #+#             */
-/*   Updated: 2024/05/28 15:43:06 by stopp            ###   ########.fr       */
+/*   Updated: 2024/05/28 17:19:08 by stopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ char	*create_heredoc(char *str, char *cmd_str, t_tree *tree, int *ex_st)
 	char	*new_cmdstr;
 
 	buf = NULL;
+	(void)ex_st;
 	if (pipe(fd) == -1)
 		return (NULL);
 	pid = fork();
@@ -77,15 +78,17 @@ char	*create_heredoc(char *str, char *cmd_str, t_tree *tree, int *ex_st)
 				break ;
 			if (ft_strncmp(buf, str, ft_strlen(buf)) == 10)
 				break ;
+			write(fd[1], buf, ft_strlen(buf));
 			free(buf);
 		}
 		free(buf);
+		free(cmd_str);
 		exit (tree->exit_status);
 	}
 	waitpid(pid, &tree->exit_status, 0);
 	if (WIFEXITED(tree->exit_status))
 		tree->exit_status = WEXITSTATUS(tree->exit_status);
-	printf("tree: %d ex: %d\n", tree->exit_status, *ex_st);
+	// printf("tree: %d ex: %d\n", tree->exit_status, *ex_st);
 	close(fd[1]);
 	tree->in_fd = fd[0];
 	new_cmdstr = create_str(cmd_str, str);
